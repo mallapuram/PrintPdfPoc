9259c474ecfdc56a9ffc8ac7da8e5be911f6ccfe
Murali Krishna Reddy Mallapuram <mmallapuram@evoketechnologies.com>
2023-02-11 16:27:03 +0530

    Initial Commit

---
+++ sampleData.js
@@ -0,0 +1,95 @@
+var printTemplate = {
+    "isDataLoaded": true,
+    "data": {
+      "AdjustmentOrder": null,
+      "MaterialCode": "B13231781         ",
+      "ProductCode": null,
+      "SubCode": null,
+      "Batch": "10048239",
+      "Color": null,
+      "TankCode": "QM000004  ",
+      "Plant": null,
+      "CreatedBy": "Murali               Krishna Reddy       (fc5558)",
+      "Version": null,
+      "InspectionLot": "030000012773",
+      "NotificationNumber": null,
+      "PlannedBatchVolume": null,
+      "ActualBatchVolume": "916.780",
+      "ProcessOrder": " 1030621",
+      "Phase": "3960",
+      "PhysicalSampleId": "000200042012",
+      "DeviationCounter": "T0",
+      "CommentsHeader": "AxCS Generated 950099884",
+      "Ingredients": [
+        {
+          "IngredientMaterialCode": "D10325583         ",
+          "Quantity": "6.0000",
+          "AdjustmentPercentage": "0.6545",
+          "Unit": "LB",
+          "IngredientDescription": "D10325583             G- 1438 UNKNOWN",
+          "SCode": ""
+        },
+        {
+          "IngredientMaterialCode": "D10220945         ",
+          "Quantity": "4.0000",
+          "AdjustmentPercentage": "0.4363",
+          "Unit": "LB",
+          "IngredientDescription": "D10220945             G- 1698 UNKNOWN",
+          "SCode": ""
+        },
+        {
+          "IngredientMaterialCode": "D11569454         ",
+          "Quantity": "5.0000",
+          "AdjustmentPercentage": "0.5454",
+          "Unit": "LB",
+          "IngredientDescription": "D11569454             G-36773 Clear",
+          "SCode": ""
+        },
+        {
+          "IngredientMaterialCode": "D10232982         ",
+          "Quantity": "2.0000",
+          "AdjustmentPercentage": "0.2182",
+          "Unit": "LB",
+          "IngredientDescription": "D10232982             H-  508 Clear",
+          "SCode": ""
+        },
+        {
+          "IngredientMaterialCode": "D10894051         ",
+          "Quantity": "3.0000",
+          "AdjustmentPercentage": "0.3272",
+          "Unit": "LB",
+          "IngredientDescription": "D10894051             H-  936 UNKNOWN",
+          "SCode": ""
+        },
+        {
+          "IngredientMaterialCode": "M20000592         ",
+          "Quantity": "1.0000",
+          "AdjustmentPercentage": "0.1091",
+          "Unit": "LB",
+          "IngredientDescription": "M20000592            RCS47621 UNKNOWN",
+          "SCode": ""
+        }
+      ],
+      "Comments": [
+        "Run At-Line Tests: Viscosity Color.",
+        "",
+        "This is a test for Line 1 text which should end here.                   ",
+        "This is a test for Line 2 text which should end here.                   ",
+        "This is a test for Line 3 text    --------------- which should end here.",
+        "This is a test for Line 4 text    --------------- which should end here.",
+        "This is a test for Line 5 text which should end here.                   ",
+        "This is a test for Line 6 text    --------------- which should end here.",
+        "AxCS Generated 950099884 Hit-1"
+      ],
+      "date": "Wed Feb 08 2023 16:31:38 ",
+      "adjmntOrdr": "T0 Hit 1"
+    },
+    'getBcode': function (ingredient) {
+        var processOrder = ('000000000000' + $.trim(this.data.ProcessOrder || '')).slice(-12);
+        var materialCode = $.trim(ingredient.IngredientMaterialCode || '');
+        var quantity = parseFloat(ingredient.Quantity || '0').toFixed(3);
+        var bcode = processOrder + materialCode + quantity;
+        return bcode;
+    },
+    'downloadFileName' : 'Hit_Ticket.pdf'
+};
